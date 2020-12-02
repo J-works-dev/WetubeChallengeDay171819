b8a25d2317f31ba1b5bc8882bc2eb4dad14fc576
SangJoon Lee(Jeremy) <sangjoon.jeremy.lee@gmail.com>
2020-12-02 12:25:13 +0800

    Set up default create default pages

--- src/movieController.js
+++ src/movieController.js
@@ -6,4 +6,42 @@ You can do Movie.find() or whatever you need like normal!
 */
 import Movie from "./models/Movie";
 
-// Add your magic here!
+export const home = async (req, res) => {
+  // res.render("home", { movies: await Movie.find(), pageTitle: "Home" });
+  res.render("home", { pageTitle: "Home" });
+};
+
+// export const movieDetail = async (req, res) => {
+//   const {
+//     params: { id }
+//   } = req;
+//   const movie = await Movie.findById(id);
+//   if (!movie) {
+//     res.render("404", { pageTitle: "Movie not found" });
+//   }
+//   return res.render("detail", { movie });
+// };
+
+export const getCreateMovie = (req, res) =>
+  res.render("create", { pageTitle: "Add Movie" });
+
+export const postCreateMovie = (req, res) => {
+  // const {
+  //   body: { title, synopsis, genres }
+  // } = req;
+  // const genreArray = genres.trim().split(",");
+  // console.log(genreArray);
+  // const movie = { title, synopsis, genres: genreArray };
+  // addMovie(movie);
+  res.redirect("/");
+};
+
+export const getEdit = (req, res) => {
+  res.render("edit", { pageTitle: "Edit Movie" });
+};
+export const postEdit = (req, res) => {
+  res.redirect("detail");
+};
+export const search = (req, res) => {
+  res.render("search", { pageTitle: "Search" });
+};
--- src/movieRouter.js
+++ src/movieRouter.js
@@ -1,7 +1,25 @@
 import express from "express";
+import {
+  home,
+  movieDetail,
+  getCreateMovie,
+  postCreateMovie,
+  getEdit,
+  postEdit,
+  search
+} from "./movieController";
 
 const movieRouter = express.Router();
 
-// Add your magic here!
+movieRouter.get("/", home);
+
+movieRouter.get("/create", getCreateMovie);
+movieRouter.post("/create", postCreateMovie);
+
+movieRouter.get("/:id", movieDetail);
+movieRouter.get("/:id/edit", getEdit);
+movieRouter.post("/:id/edit", postEdit);
+
+movieRouter.get("/search", search);
 
 export default movieRouter;
